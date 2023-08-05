c792fd25325cd4340aa8835a626ece4d80f25502
Honey2339 <prasoon2study@gmail.com>
2023-08-05 10:08:33 +0530

    Fixed Some Bugs

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -11,6 +11,7 @@ import {
   StackDivider,
   ChakraProvider,
   theme,
+  Stack,
 } from "@chakra-ui/react"
 import React from "react"
 import { Link } from "react-router-dom"
@@ -22,12 +23,18 @@ function About() {
     <div>
       <ChakraProvider theme={theme} resetCSS={false}>
         <Box className="about-container" mt="280px" ml="120px">
-          <Text fontFamily="Poppins" fontSize="27px" fontWeight="500">
+          <Text
+            fontFamily="Poppins"
+            fontSize="27px"
+            fontWeight="500"
+            className="aboutus"
+          >
             About Us
           </Text>
           <Text
+            className="para"
             fontFamily="Poppins"
-            fontSize="17px"
+            fontSize={["12px", "14px", "16px", "18px"]}
             maxW="500px"
             fontWeight="500"
           >
@@ -37,88 +44,97 @@ function About() {
             information before hand.
           </Text>
         </Box>
-        <motion.div
-          initial={{ opacity: 0, scale: 1 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 0.8,
-            delay: 1.5,
-            ease: [0, 0.71, 0.2, 1.01],
-          }}
+        <Stack
+          direction={["column", "row"]}
+          spacing={4}
+          mt={4}
+          ml={["20px", "120px"]}
         >
-          <Card
-            className="cardbody-container"
-            position="absolute"
-            top="250px"
-            right="800px"
-            maxW="290px"
-            bg="whiteAlpha.1100"
-            borderRadius="10px"
-            border="1px solid black"
+          <motion.div
+            className="prasoon-container"
+            initial={{ opacity: 0, scale: 1 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 1.5,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
           >
-            <CardHeader>
-              <Heading color="black">Prasoon</Heading>
-            </CardHeader>
-            <Divider />
-            <CardBody color="black">
-              <Text>
-                Im a MERN Stack Developer, Im looking forward to do data
-                structure and algorithms in C++
-              </Text>
-            </CardBody>
-            <CardFooter>
-              <Button colorScheme="orange" border="0">
-                <Link
-                  className="anchor"
-                  to="https://prasoonportfolio.netlify.app"
-                >
-                  Portfolio
-                </Link>
-              </Button>
-            </CardFooter>
-          </Card>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, scale: 1 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 0.8,
-            delay: 2,
-            ease: [0, 0.71, 0.2, 1.01],
-          }}
-        >
-          <Card
-            className="cardbody-container"
-            position="absolute"
-            top="250px"
-            right="350px"
-            maxW="290px"
-            bg="whiteAlpha.1100"
-            borderRadius="10px"
-            border="1px solid black"
+            <Card
+              className="cardbody-container"
+              position="absolute"
+              top="250px"
+              right="800px"
+              maxW="290px"
+              bg="whiteAlpha.1100"
+              borderRadius="10px"
+              border="1px solid black"
+            >
+              <CardHeader>
+                <Heading color="black">Prasoon</Heading>
+              </CardHeader>
+              <Divider />
+              <CardBody color="black">
+                <Text>
+                  Im a MERN Stack Developer, Im looking forward to do data
+                  structure and algorithms in C++
+                </Text>
+              </CardBody>
+              <CardFooter>
+                <Button colorScheme="orange" border="0">
+                  <Link
+                    className="anchor"
+                    to="https://prasoonportfolio.netlify.app"
+                  >
+                    Portfolio
+                  </Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          </motion.div>
+          <motion.div
+            className="soma-container"
+            initial={{ opacity: 0, scale: 1 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 2,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
           >
-            <CardHeader>
-              <Heading color="black">Soma Akash</Heading>
-            </CardHeader>
-            <Divider />
-            <CardBody color="black">
-              <Text>
-                Im a newbie and i feel like im good in web development and
-                exploring the world of coding and im looking for opportunites
-              </Text>
-            </CardBody>
-            <CardFooter>
-              <Button colorScheme="blue" border="0">
-                <Link
-                  className="anchor"
-                  to="https://www.linkedin.com/in/soma-akash-perni-2432a7185/"
-                >
-                  LinkedIn
-                </Link>
-              </Button>
-            </CardFooter>
-          </Card>
-        </motion.div>
+            <Card
+              className="cardbody-container"
+              position="absolute"
+              top="250px"
+              right="350px"
+              maxW="290px"
+              bg="whiteAlpha.1100"
+              borderRadius="10px"
+              border="1px solid black"
+            >
+              <CardHeader>
+                <Heading color="black">Soma Akash</Heading>
+              </CardHeader>
+              <Divider />
+              <CardBody color="black">
+                <Text>
+                  Im a newbie and i feel like im good in web development and
+                  exploring the world of coding and im looking for opportunites
+                </Text>
+              </CardBody>
+              <CardFooter>
+                <Button colorScheme="blue" border="0">
+                  <Link
+                    className="anchor"
+                    to="https://www.linkedin.com/in/soma-akash-perni-2432a7185/"
+                  >
+                    LinkedIn
+                  </Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          </motion.div>
+        </Stack>
       </ChakraProvider>
     </div>
   )
